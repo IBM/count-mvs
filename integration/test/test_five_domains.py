10f66b0fb1cbd847c04f454bf357ecc843387d9b
michael.benson <mbenson@uk.ibm.com>
2022-03-08 11:49:49 +0000

    Improve yapf formatting

--- integration/test/test_five_domains.py
+++ integration/test/test_five_domains.py
@@ -10,95 +10,41 @@ from util.db import Database
 from util.api import API
 from util.log import print_count_mvs_log
 
-domains = [{
-    "id": 0,
-    "name": "test1",
-}, {
-    "id": 1,
-    "name": "test2",
-}, {
-    "id": 2,
-    "name": "test3",
-}, {
-    "id": 3,
-    "name": "test4",
-}, {
-    "id": 4,
-    "name": "test5",
-}]
+domains = [
+    {"id": 0, "name": "test1"},
+    {"id": 1, "name": "test2"},
+    {"id": 2, "name": "test3"},
+    {"id": 3, "name": "test4"},
+    {"id": 4, "name": "test5"},
+]
 
-sensor_devices = [{
-    "id": 0,
-    "hostname": "localhost",
-    "devicename": "test1",
-    "devicetypeid": 0,
-    "spconfig": 0
-}, {
-    "id": 1,
-    "hostname": "qradar",
-    "devicename": "test2",
-    "devicetypeid": 0,
-    "spconfig": 1
-}, {
-    "id": 2,
-    "hostname": "ibm.com",
-    "devicename": "test3",
-    "devicetypeid": 0,
-    "spconfig": 2
-}, {
-    "id": 3,
-    "hostname": "example.com",
-    "devicename": "test4",
-    "devicetypeid": 0,
-    "spconfig": 3
-}, {
-    "id": 4,
-    "hostname": "test.com",
-    "devicename": "test5",
-    "devicetypeid": 0,
-    "spconfig": 4
-}]
+sensor_devices = [
+    {"id": 0, "hostname": "localhost", "devicename": "test1", "devicetypeid": 0, "spconfig": 0},
+    {"id": 1, "hostname": "qradar", "devicename": "test2", "devicetypeid": 0, "spconfig": 1},
+    {"id": 2, "hostname": "ibm.com", "devicename": "test3", "devicetypeid": 0, "spconfig": 2},
+    {"id": 3, "hostname": "example.com", "devicename": "test4", "devicetypeid": 0, "spconfig": 3},
+    {"id": 4, "hostname": "test.com", "devicename": "test5", "devicetypeid": 0, "spconfig": 4},
+]
 
-sensor_protocol_configs = [{
-    "id": 0,
-    "spid": 0
-}, {
-    "id": 1,
-    "spid": 1
-}, {
-    "id": 2,
-    "spid": 2
-}, {
-    "id": 3,
-    "spid": 3
-}, {
-    "id": 4,
-    "spid": 4
-}]
+sensor_protocol_configs = [
+    {"id": 0, "spid": 0},
+    {"id": 1, "spid": 1},
+    {"id": 2, "spid": 2},
+    {"id": 3, "spid": 3},
+    {"id": 4, "spid": 4},
+]
 
-sensor_protocol_config_parameters = [{"id": 0, "sensorprotocolconfigid": 0, "name": "server", "value": "localhost"}]
+sensor_protocol_config_parameters = [
+    {"id": 0, "sensorprotocolconfigid": 0, "name": "server", "value": "localhost"},
+]
 
-search_data = [{
-    "events": [{
-        "domain_id": 0,
-    }]
-}, {
-    "events": [{
-        "domain_id": 1,
-    }]
-}, {
-    "events": [{
-        "domain_id": 2,
-    }]
-}, {
-    "events": [{
-        "domain_id": 3,
-    }]
-}, {
-    "events": [{
-        "domain_id": 4,
-    }]
-}]
+search_data = [
+    {"events": [{"domain_id": 0}]},
+    {"events": [{"domain_id": 1}]},
+    {"events": [{"domain_id": 2}]},
+    {"events": [{"domain_id": 3}]},
+    {"events": [{"domain_id": 4}]},
+]
 
 
 def do_setup():
